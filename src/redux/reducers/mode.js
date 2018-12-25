9cf0cb6d520fbaa71d5a155ad3df4d5e7dadbd7a
Benjamin <benjamin10882579@yahoo.com>
2018-12-24 19:25:59 -0800

    mode reducer was added

---
+++ src/redux/reducers/mode.js
@@ -0,0 +1,12 @@
+const initialState  = {
+  server: '',
+}
+
+
+
+const reducer = (state = initialState, action) => {
+
+  return state
+}
+
+export default reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import home from './reducers/home';
+import mode from './reducers/mode';
 
 const reducer = combineReducers(
   {
-    home: home
+    home: home,
+    mode: mode,
   }
 )
 
