56355102ca2d72420cc00ca78112287e3f3cff24
Benjamin <benjamin10882579@yahoo.com>
2018-12-28 20:50:20 -0800

    Navigating to last screen with handleGoBack implemented

--- src/redux/actions/handleGoBack.js
+++ src/redux/actions/handleGoBack.js
@@ -1,6 +1,20 @@
 const handleGoBack = (nav) => {
   return (dispatch, getState) => {
-    nav.navigate('Home')
+    const { mode } = getState();
+    const { screen } = mode;
+
+    if(screen.length > 0){
+      if (screen[screen.length - 1].direction == 'Post' || screen[screen.length - 1].direction == 'Account'){
+        nav.navigate(screen[screen.length - 1].direction, {id: screen[screen.length - 1].id})
+      }
+      else{
+        nav.navigate(screen[screen.length - 1].direction, {page: 1})
+      }
+      dispatch({type: 'POP_LAST_SCREEN'})
+    }
+    else{
+      nav.navigate('Home')
+    }
   }
 }
 
