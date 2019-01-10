e6e886f93eeadfd5c3310c118961e65655a933ef
Benjamin <benjamin10882579@yahoo.com>
2019-01-09 20:55:15 -0800

    Fetch more posts at the end of the scrollview

--- src/screens/account/components/userPosts.js
+++ src/screens/account/components/userPosts.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Image, FlatList } from 'react-native';
+import { Text, View, TouchableOpacity, Image, FlatList, Animated } from 'react-native';
 import { Ionicons, Feather } from '@expo/vector-icons';
 
 import { defaultStyle, accountStyle } from '@src/static/index';
 
-export default (props) => {
+export default class App extends Component{
 
-  const { posts, navigation, fetchPost, updateNavState } = props;
+  state = {
+    page: 1,
+  }
+
+  componentDidMount(){
+    const { account, fetchUserPosts } = this.props;
+    fetchUserPosts(account.account_id, this.state.page)
+  }
 
   handleFetchPost = (id) => {
+    const { navigation, fetchPost, updateNavState } = this.props;
     updateNavState({direction: 'Account'});
     fetchPost(id, (status) => {
       if(status === 200){
@@ -17,32 +25,52 @@ export default (props) => {
     })
   }
 
-  return (
-    <FlatList
-      data={posts}
-      scrollEventThrottle={1}
-      bounces={false}
-      // onScroll={ (e) => fetchPostOnScroll(e.nativeEvent) }
-      renderItem={ ({item, index}) => (
-        <TouchableOpacity
-          activeOpacity={0.9}
-          style={[accountStyle.listItem, defaultStyle.shadow]}
-          onPress={ () => this.handleFetchPost(item.id) }
-        >
-          <View style={accountStyle.itemImageContainer}>
-            <Image source={{uri: item.photos[0].uri}} style={defaultStyle.image}/>
-          </View>
-          <View style={accountStyle.itemInformation}>
-            <View style={accountStyle.itemTitleContainer}>
-              <Text style={accountStyle.itemTitle} numberOfLines={2}>{item.title}</Text>
+  fetchPostOnScroll = (e) => {
+    const { layoutMeasurement, contentOffset, contentSize } = e;
+    const { account, fetchUserPosts } = this.props;
+
+    if(layoutMeasurement.height + contentOffset.y == (contentSize.height) ){
+      this.setState((prev) => ({...prev, page: prev.page +1}), () => {
+        fetchUserPosts(account.account_id, this.state.page)
+      })
+    }
+  }
+
+  render(){
+    const { posts, position } = this.props;
+
+    return (
+      <FlatList
+        data={posts}
+        scrollEventThrottle={1}
+        onScroll={
+          Animated.event(
+            [{nativeEvent: {contentOffset: {y: position}} }],
+            {listener: (e) => this.fetchPostOnScroll(e.nativeEvent) },
+          )
+        }
+        renderItem={ ({item, index}) => (
+          <TouchableOpacity
+            activeOpacity={0.9}
+            style={[accountStyle.listItem, defaultStyle.shadow]}
+            onPress={ () => this.handleFetchPost(item.id) }
+          >
+            <View style={accountStyle.itemImageContainer}>
+              <Image source={{uri: item.photos[0].uri}} style={defaultStyle.image}/>
             </View>
-            <View style={accountStyle.itemPriceContainer}>
-              <Text style={accountStyle.itemPrice} numberOfLines={1}>{item.price}</Text>
+            <View style={accountStyle.itemInformation}>
+              <View style={accountStyle.itemTitleContainer}>
+                <Text style={accountStyle.itemTitle} numberOfLines={2}>{item.title}</Text>
+              </View>
+              <View style={accountStyle.itemPriceContainer}>
+                <Text style={accountStyle.itemPrice} numberOfLines={1}>{item.price}</Text>
+              </View>
             </View>
-          </View>
-        </TouchableOpacity>
-      )}
-      keyExtractor={ (item, index) => String(item.id)}
-    />
-  )
+          </TouchableOpacity>
+        )}
+        keyExtractor={ (item, index) => String(item.id)}
+      />
+    )
+  }
+
 }
