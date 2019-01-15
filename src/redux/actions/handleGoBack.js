cfd2e303c06f3e97d31207e944bf2e6423c91dbb
Benjamin <benjamin10882579@yahoo.com>
2019-01-15 04:09:46 -0800

    handleGoBack bug fixed

--- src/redux/actions/handleGoBack.js
+++ src/redux/actions/handleGoBack.js
@@ -1,11 +1,21 @@
+import { fetchPost } from '@src/requests';
+
 const handleGoBack = (nav) => {
   return (dispatch, getState) => {
-    const { mode } = getState();
+    const { mode, account } = getState();
     const { screen } = mode;
 
     if(screen.length > 0){
-      if (screen[screen.length - 1].direction == 'Post' || screen[screen.length - 1].direction == 'AccountBYId'){
-        nav.navigate(screen[screen.length - 1].direction, {id: screen[screen.length - 1].id})
+      if (screen[screen.length - 1].direction == 'Post'){
+        fetchPost({
+          mode: mode.server,
+          token: account.token,
+          id: screen[screen.length - 1].id
+        })
+        .then( (data) => {
+          dispatch({type: 'ERASE_CREATE_DATA_STATE'})
+          nav.navigate('Post', {...data})
+        })
       }
       else{
         nav.navigate(screen[screen.length - 1].direction, {page: 1})
